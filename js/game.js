cf646a85314d370e39056404e06b8f27b8c3a0e0
liaoshibiao <2468442276@qq.com>
2018-08-30 15:35:35 +0800

    1

---
+++ js/game.js
@@ -0,0 +1,34 @@
+
+	$(".ball").click(function(){
+		window.location="http://h5.wenmall.cn/lu/h5/football/"
+	})
+	$(".wx").click(function(){
+		window.location="http://h5.wenmall.cn/lu/games/97/"
+	})
+	$(".llk").click(function(){
+		window.location="http://h5.wenmall.cn/lu/games/llk12/"
+	})
+	$(".two").click(function(){
+		window.location="http://h5.wenmall.cn/lu/games/2048JD/"
+	})
+	$(".foot").click(function(){
+		alert("尚未更多游戏")
+	})
+	
+
+
+//window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", hengshuping, false);
+//window.onload=function(){
+//	hengshuping()
+//}
+// function hengshuping() {
+//      if (window.orientation == 90 || window.orientation == -90) {
+//       	 
+//       	 $(".B").show()//竖屏
+//       	 
+//      } else {
+//      	
+//      	$(".B").hide()//横屏
+//     
+//      }
+//  }
